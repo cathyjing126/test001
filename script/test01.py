85249d51e0207eb18dcf8bfdeba47e14f8886b0a
cathyjing126 <cathy_jing126@163.com>
2018-05-27 16:33:13 +0800

    这是新提交的文件

--- script/test01.py
+++ script/test01.py
@@ -1,6 +1,14 @@
 ﻿import pytest,allure
 class Test001:
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
     @allure.step(title="测试步骤01")
     @pytest.mark.parametrize("a",[1,2,3])
     def test_001(self,a):
+        allure.attach("描述","这是测试步骤01的描述...")
+        assert a !=2
+
+    @allure.step(title="测试步骤02")
+    @pytest.mark.parametrize("a", [1, 2, 3])
+    def test_002(self,a):
+        allure.attach("描述","这是测试步骤02的描述...")
         assert a !=2
