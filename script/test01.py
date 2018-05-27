00157646b667b69b5adb4176add2d7b245be7bdb
cathyjing126 <cathy_jing126@163.com>
2018-05-27 16:24:25 +0800

    这是新提交的文件

--- script/test01.py
+++ script/test01.py
@@ -1,6 +1,6 @@
-import pytest,allure
+﻿import pytest,allure
 class Test001:
-	@allure.step(title="测试步骤01")
+    @allure.step(title="测试步骤01")
     @pytest.mark.parametrize("a",[1,2,3])
     def test_001(self,a):
         assert a !=2
